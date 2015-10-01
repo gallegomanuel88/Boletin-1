d945642786a427620ae2548522e20f4d15492a95
manu <manu@manu-HP-Pavilion-g6-Notebook-PC>
2015-10-01 09:48:23 +0200

    Modificado comentario

---
+++ src/pruebas/con/git/pkg2/PruebasConGIT2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebas.con.git.pkg2;
+
+/**
+ *
+ * @author manu
+ */
+public class PruebasConGIT2 {
+
+    /**
+     * empezando
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
